71d93995846d92f5f4c2e0c4b5c183a8e8d81a71
Andrew1232 <celms.egils@gmail.com>
2022-04-13 20:05:45 +0300

    graph test 3

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 #<meta http-equiv="X-UA-Compatible" content="IE=edge">
-@app.route("/",methods=['GET'])
+@app.route("/")#,methods=['GET']
 def home():
     data=[
-        ("1",1),
+        ("1",2),
         ("2",1),
         ("3",1),
         ("4",1),
-        ("5",1),
-        ("6",1),
-        ("7",1)
+        ("5",4),
+        ("6",0),
+        ("7",13)
     ]
 
     labels=[row[0]for row in data]
