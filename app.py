49c9fecbaa301166e472c1d643693365851b6634
Andrew1232 <celms.egils@gmail.com>
2022-04-13 18:52:03 +0300

    graph test 1

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET'])
+def home():
+    data=[
+        ("1",1),
+        ("2",1),
+        ("3",1),
+        ("4",1),
+        ("5",1),
+        ("6",1),
+        ("7",1)
+    ]
+
+    labels=[row[0]for row in data]
+    values=[row[1]for row in data]
+
+    return render_template("graph.html",labels=labels, values=values)
+
+
+if __name__ == '__main__':
+  app.run()
